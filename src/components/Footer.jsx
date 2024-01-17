4b03c6f5373c0498b6da8ae4d824ab9848fbb571
ismail <ismailghedamsi@gmail.com>
2024-01-17 14:22:11 -0500

    Add portfolio footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import linkIcon from '../assets/transparent_link.png';
+import backgroundImage from '../assets/black_background.jpg'; // adjust the path as needed
+
 
 const Footer = () => {
     const linkStyles = {
@@ -22,8 +24,9 @@ const Footer = () => {
 
     const socialLinkStyles = {
         fontSize: '16px',
-        color: '#333',
+        color: 'gold',
         display: "flex",
+        textDecoration: 'none',
         justifyContent: "space-around",
         '@media (max-width: 600px)': {
             flexDirection: 'row',
@@ -36,7 +39,8 @@ const Footer = () => {
     };
 
     const footerStyles = {
-        backgroundColor: '#f2f2f2', // light grey
+        backgroundColor: 'BLACK',
+        color: 'GOLD',
         width: '100%',
         display: 'flex', // Add this line
         justifyContent: 'center', // Add this line
@@ -50,13 +54,13 @@ const Footer = () => {
             <div className="footer-container">
                 <p>&copy; {new Date().getFullYear()} Ismail Ghedamsi</p>
                 <div className="social-links">
-                    <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer" style={linkStyles}>
+                    <a href="https://www.linkedin.com/in/ismail-ghedamsi-29997540/" target="_blank" rel="noopener noreferrer" style={linkStyles}>
                     <div style={flexContainerStyles}>
                         <img src={linkIcon} alt="Linkedin" style={{ width: '40px', height: '40px', marginRight: '5px' }} />
                         <span style={socialLinkStyles}>Linkedin</span>
                     </div>
                     </a>
-                    <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer" style={linkStyles}>
+                    <a href="https://github.com/ismailghedamsi" target="_blank" rel="noopener noreferrer" style={linkStyles}>
                     <div style={flexContainerStyles}>
                         <img src={linkIcon} alt="Github" style={{ width: '40px', height: '40px', marginRight: '5px' }} />
                         <span style={socialLinkStyles}>Github</span>
